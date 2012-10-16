4e25c3426da952ca1acdef1561b721c0099157c9
Solomon Foster <colomon@gmail.com>
2012-10-16 08:34:48 -0400

    Reformat code.

--- lib/ISO_10303_21/Actions.pm
+++ lib/ISO_10303_21/Actions.pm
@@ -8,14 +8,10 @@ sub merge-arrays(@arrays) {
 
 class ISO_10303_21::Actions {
     method entity_instance_name($/) { make [~$/] }
-    method parameter($/) {
-        make $/.values[0].ast;
-    }
+    method parameter($/) { make $/.values[0].ast }
     method omitted_parameter($/) { make [] }
-    method untyped_parameter($/) {
-        make $/.values[0].ast // [];
-    }
+    method untyped_parameter($/) { make $/.values[0].ast // [] }
     method typed_parameter($/) { make $<parameter>.ast }
-    method list_of_parameters($/) { make merge-arrays(@($<parameter>)».ast); }
-    method parameter_list($/) { make merge-arrays(@($<parameter>)».ast); }
+    method list_of_parameters($/) { make merge-arrays(@($<parameter>)».ast) }
+    method parameter_list($/) { make merge-arrays(@($<parameter>)».ast) }
 }
