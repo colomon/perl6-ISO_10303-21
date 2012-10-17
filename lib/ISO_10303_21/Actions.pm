19330cbd524eab65cce66aab76d0e48eae465e6e
Solomon Foster <colomon@gmail.com>
2012-10-17 15:57:31 -0400

    Get rid of that stupid merge-arrays sub.

--- lib/ISO_10303_21/Actions.pm
+++ lib/ISO_10303_21/Actions.pm
@@ -1,17 +1,12 @@
 use v6;
 use ISO_10303_21::Grammar;
 
-sub merge-arrays(@arrays) {
-    my @result = @arrays.map(*.list);
-    @result;
-}
-
 class ISO_10303_21::Actions {
     method entity_instance_name($/) { make [~$/] }
     method parameter($/) { make $/.values[0].ast }
     method omitted_parameter($/) { make [] }
     method untyped_parameter($/) { make $/.values[0].ast // [] }
     method typed_parameter($/) { make $<parameter>.ast }
-    method list_of_parameters($/) { make merge-arrays(@($<parameter>)».ast) }
-    method parameter_list($/) { make merge-arrays(@($<parameter>)».ast) }
+    method list_of_parameters($/) { make @($<parameter>)».ast.map(*.list).Array }
+    method parameter_list($/) { make @($<parameter>)».ast.map(*.list).Array }
 }
