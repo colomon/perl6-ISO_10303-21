2526ef0eedfc6cbaa2f3408edb455118c45f865b
Solomon Foster <colomon@gmail.com>
2013-12-06 23:02:54 -0500

    Uppercase hex.

--- lib/ISO_10303_21/Utils.pm
+++ lib/ISO_10303_21/Utils.pm
@@ -3,11 +3,11 @@ use v6;
 package ISO_10303_21::Utils {
     sub EncodeString(Str $string) is export {
         "'" ~ $string.ords.map({
-            when * < 32    { '\\X\\' ~ sprintf("%02x", $_) }
+            when * < 32    { '\\X\\' ~ sprintf("%02X", $_) }
             when * < 128   { .chr }
-            when * < 256   { '\\X\\' ~ sprintf("%02x", $_) }
-            when * < 65535 { '\\X2\\' ~ sprintf("%04x", $_) ~ '\\X0\\' }
-            default        { '\\X4\\' ~ sprintf("%08x", $_) ~ '\\X0\\' }
+            when * < 256   { '\\X\\' ~ sprintf("%02X", $_) }
+            when * < 65535 { '\\X2\\' ~ sprintf("%04X", $_) ~ '\\X0\\' }
+            default        { '\\X4\\' ~ sprintf("%08X", $_) ~ '\\X0\\' }
         }).join ~ "'";
     }
 }
