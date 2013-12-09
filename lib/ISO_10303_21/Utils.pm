12134c79371b08646461a587ea22f62d0c644c30
Solomon Foster <colomon@gmail.com>
2013-12-08 22:00:12 -0500

    EncodeString compression.

--- lib/ISO_10303_21/Utils.pm
+++ lib/ISO_10303_21/Utils.pm
@@ -2,13 +2,37 @@ use v6;
 
 package ISO_10303_21::Utils {
     sub EncodeString(Str $string) is export {
-        "'" ~ $string.ords.map({
+        my @codes := $string.ords.map({
             when * < 32    { '\\X\\' ~ sprintf("%02X", $_) }
             when * < 128   { .chr }
             when * < 256   { '\\X\\' ~ sprintf("%02X", $_) }
-            when * < 65535 { '\\X2\\' ~ sprintf("%04X", $_) ~ '\\X0\\' }
-            default        { '\\X4\\' ~ sprintf("%08X", $_) ~ '\\X0\\' }
-        }).join ~ "'";
+            when * < 65535 { "X2" => sprintf("%04X", $_) }
+            default        { "X4" => sprintf("%08X", $_) }
+        });
+
+        my $result = "'";
+        my $state = "";
+        sub close-state { $result ~= "\\X0\\" if $state; }
+
+        for @codes {
+            when Str {
+                close-state;
+                $state = "";
+                $result ~= $_;
+            }
+            when Pair {
+                if .key eq $state {
+                    $result ~= .value;
+                } else {
+                    close-state;
+                    $state = .key;
+                    $result ~= "\\$state\\" ~ .value;
+                }
+            }
+        }
+        close-state;
+        
+        $result ~ "'";
     }
 }
 
